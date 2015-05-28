217289bdb6ee678475545270fa7967e3dad9a395
vlad.kaplarevic@gmail.com <vlad.kaplarevic@gmail.com>
2015-05-28 19:03:31 +0200

    Modified tabs...

--- perl/code-eval-template.pl
+++ perl/code-eval-template.pl
@@ -7,14 +7,14 @@ use v5.10;
 
 sub main
 {
-	open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
-	while(my $line = <INFILE>) 
-	{
-    	next if($line =~ m/^\s$/);  # skip blank lines
-	    $line =~ s/(^\s|\s*$)//g;   # remove leading and trailing spaces
-		# ... do someting with the line...	
-	}
-	close(INFILE);
+    open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+    while(my $line = <INFILE>) 
+    {
+        next if($line =~ m/^\s$/);  # skip blank lines
+        $line =~ s/(^\s|\s*$)//g;   # remove leading and trailing spaces
+        # ... do someting with the line...	
+    }
+    close(INFILE);
 }
 
 main();
